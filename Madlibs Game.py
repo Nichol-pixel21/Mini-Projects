80e2bfe487d786335c519da4b5475cca90a071a2
Nichol-pixel21 <nicholethfernandes@gmail.com>
2025-10-27 18:37:08 +0530

    Add files via upload
    My first ever coding project !

---
+++ Madlibs Game.py
@@ -0,0 +1,24 @@
+age = int(input('Enter your age :'))
+
+if age >= 18 :
+    print("You are now signed up,, start your journey today.")
+elif age <= 0 :
+    print("You are too young to sign up =, sweetheart. Better luck next time.")
+elif age >= 200 : 
+    print("DAMN, great job on maintainging your health but too bad you can't sign up sweety.")
+else age = 1010 :
+    print(" How are you even alive ?? good job")
+
+response = input("Would you like some food ?")
+
+if response == "Y" :
+    print("YAY, go cook ramen and on for me too!")
+else reponse == "N"
+    print("BYe")
+
+online = True 
+
+if online :
+    print("Let's play or chat.")
+else :
+    print("I'm so lonely.....s")
